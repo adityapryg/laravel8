5af1252dc2c7f6e12a52365bb65c228360f17df4
adityapryg <aditya@brainmatics.com>
2020-12-02 11:09:09 +0700

    first commit

---
+++ app/Charts/SampleChart.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Charts;
+
+use Chartisan\PHP\Chartisan;
+use ConsoleTVs\Charts\BaseChart;
+use Illuminate\Http\Request;
+
+class SampleChart extends BaseChart
+{
+    /**
+     * Handles the HTTP request for the given chart.
+     * It must always return an instance of Chartisan
+     * and never a string or an array.
+     */
+    public function handler(Request $request): Chartisan
+    {
+        return Chartisan::build()
+            ->labels(['直达', '营销广告', '搜索引擎', '邮件营销', '联盟广告', '视频广告', '百度', '谷歌', '必应', '其他'])
+            ->dataset('Dataset Luar', [335, 310, 234, 135, 1048, 251, 147, 102])
+            ->dataset('Dataset Dalam', [335, 679, 1548]);
+    }
+}
---
+++ resources/views/sample_chart.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Chartisan example</title>
+  </head>
+  <body>
+    <!-- Chart's container -->
+    <div id="chart" style="height: 300px;"></div>
+    <!-- Charting library -->
+    <script src="https://unpkg.com/echarts/dist/echarts.min.js"></script>
+    <!-- Chartisan -->
+    <script src="https://unpkg.com/@chartisan/echarts/dist/chartisan_echarts.js"></script>
+    <!-- Your application script -->
+    <script>
+      const chart = new Chartisan({
+        el: '#chart',
+        url: "@chart('sample_chart')",
+        hooks: new ChartisanHooks()
+        .legend()
+        .colors()
+        .tooltip()
+        .axis(false)
+        .datasets([
+            { type: 'pie', radius: ['40%', '60%'] },
+            { type: 'pie', radius: ['10%', '30%'] },
+        ]),
+      });
+    </script>
+  </body>
+</html>
